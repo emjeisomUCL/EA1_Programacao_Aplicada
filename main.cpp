e4ff837550308846e0d0b6ef25ab7dfdd1dbf2de
Emanuel <>
2021-04-16 20:41:06 +0000

    Feita lógica de conversão de binario para decimal

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <math.h>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  
+  int pot2=0,pos, soma=0;
+  string bin;
+
+  cout<<"Numero binario: ";
+  cin>> bin;
+
+  pos = bin.length();
+  for(int i = (bin.length() - 1); i >= 0; i--){
+    if(bin[i]=='1'){
+      soma += pow(2, pot2);
+    }
+    pos--;
+    pot2++;
+  }
+  cout<<"Binario convertido para decimal = "<<soma;
+
+  cout<<endl<<endl;
+  return 0;
 }
